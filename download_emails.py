ae25ce5ca8e2c6367b99bad30bec79aa6a1386d9
Mohammed Al-Jefairi <mohammedaljefairi@gmail.com>
2023-08-16 14:53:16 +0300

    update error handeler

--- download_emails.py
+++ download_emails.py
@@ -2,100 +2,117 @@ import imaplib
 import email
 import configparser
 import json
+import os
 from email.header import decode_header
 import ssl
-import os
+from datetime import datetime
+from datetime import timezone
 
-# Constants
+# Configurations
 CONFIG_FILE = 'config.ini'
-EMAIL_FILE = 'emails.json'
+OUTPUT_FILE = 'emails.json'
+LAST_TIMESTAMP_FILE = 'last_timestamp.txt'
 
-# Read the configuration file
+# Load configurations
 config = configparser.ConfigParser(interpolation=None)
 config.read(CONFIG_FILE)
-
 EMAIL = config['credentials']['email']
 PASSWORD = config['credentials']['password']
 IMAP_SERVER = config['server']['IMAP_SERVER']
 IMAP_PORT = int(config['server']['IMAP_PORT'])
 
-def connect_to_mailbox():
-    context = ssl.create_default_context()
+def parse_email_date(date_str):
     try:
-        mail = imaplib.IMAP4_SSL(IMAP_SERVER, IMAP_PORT, ssl_context=context)
-        mail.login(EMAIL, PASSWORD)
-        mail.select("inbox")
-        return mail
-    except imaplib.IMAP4.error:
-        print("Authentication failed. Check your email and password in the config file.")
-        return None
+        parsed_date = email.utils.parsedate_to_datetime(date_str)
+        if parsed_date.tzinfo:  # If the datetime is offset-aware
+            return parsed_date.astimezone(timezone.utc).replace(tzinfo=None)  # Convert to UTC and remove tzinfo
+        return parsed_date
     except Exception as e:
-        print(f"An error occurred: {e}")
+        print(f"Error parsing date: {e}")
         return None
-
+    
 def safe_decode(data):
     if isinstance(data, bytes):
         try:
-            return data.decode()
+            return data.decode('utf-8')
         except UnicodeDecodeError:
-            return data.decode('utf-8', 'replace')
-    return data  # if it's already a string
+            return data.decode('latin1')
+    else:
+        # Return string as is if it's already a string
+        return data
 
 
-def fetch_emails(last_email_id=None):
-    mail = connect_to_mailbox()
-    if not mail:
-        return []
+def fetch_emails(last_timestamp=None):
+    context = ssl.create_default_context()
+    mail = imaplib.IMAP4_SSL(IMAP_SERVER, IMAP_PORT, ssl_context=context)
+    mail.login(EMAIL, PASSWORD)
+    mail.select("inbox")
 
-    status, messages = mail.search(None, "ALL")
+    if last_timestamp:
+        query = f"SINCE {last_timestamp.strftime('%d-%b-%Y')}"
+        status, messages = mail.search(None, query)
+    else:
+        status, messages = mail.search(None, "ALL")
+
     email_ids = messages[0].split(b' ')
-    
-    if last_email_id:
-        start_index = email_ids.index(last_email_id) + 1
-        email_ids = email_ids[start_index:]
-
     all_emails = []
+    max_timestamp = last_timestamp
+
     for e_id in email_ids:
         _, msg_data = mail.fetch(e_id, '(RFC822)')
         for response_part in msg_data:
             if isinstance(response_part, tuple):
                 msg = email.message_from_bytes(response_part[1])
-                email_subject = decode_header(msg["Subject"])[0][0]
-                email_subject = safe_decode(email_subject)
-                email_from = decode_header(msg["From"])[0][0]
-                email_from = safe_decode(email_from)
-                payload = msg.get_payload(decode=True)
-                body = safe_decode(payload) if payload else ''
-                all_emails.append({
-                    "from": email_from,
-                    "subject": email_subject,
-                    "body": body,
-                    "id": e_id.decode()
-                })
-                print(f"Processed email ID: {e_id.decode()}")
-                # Intermediate save
-                save_to_json(all_emails, EMAIL_FILE)
+                email_subject = safe_decode(decode_header(msg["Subject"])[0][0])
+                email_from = safe_decode(decode_header(msg["From"])[0][0])
+
+                try:
+                    email_date = msg["Date"]
+                    email_datetime = datetime.strptime(email_date, '%a, %d %b %Y %H:%M:%S %z')
+                    if not max_timestamp or email_datetime.replace(tzinfo=None) > max_timestamp:
+                        max_timestamp = email_datetime
+
+                    payload = msg.get_payload(decode=True)
+                    body = safe_decode(payload) if payload else ''
+
+                    all_emails.append({
+                        "from": email_from,
+                        "subject": email_subject,
+                        "body": body,
+                        "date": email_datetime.strftime('%Y-%m-%d %H:%M:%S')
+                    })
+                except ValueError:
+                    print(f"Could not parse date for email ID: {e_id.decode()}")
 
     mail.close()
     mail.logout()
-    return all_emails
+    return all_emails, max_timestamp
+
 
 def save_to_json(data, filename):
     with open(filename, 'w', encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
 
-def load_existing_emails(filename):
+
+def save_timestamp(timestamp, filename):
+    with open(filename, 'w') as f:
+        f.write(timestamp.strftime('%Y-%m-%d %H:%M:%S'))
+
+
+def load_timestamp(filename):
     if os.path.exists(filename):
-        with open(filename, 'r', encoding="utf-8") as f:
-            return json.load(f)
-    return []
+        with open(filename, 'r') as f:
+            timestamp_str = f.read().strip()
+            return datetime.strptime(timestamp_str, '%Y-%m-%d %H:%M:%S')
+    return None
+
 
 if __name__ == "__main__":
-    existing_emails = load_existing_emails(EMAIL_FILE)
-    last_email_id = existing_emails[-1]['id'] if existing_emails else None
-    
-    new_emails = fetch_emails(last_email_id)
-
-    all_emails = existing_emails + new_emails
-    save_to_json(all_emails, EMAIL_FILE)
-    print(f"Total emails saved: {len(all_emails)}")
+    last_timestamp = load_timestamp(LAST_TIMESTAMP_FILE)
+    new_emails, new_timestamp = fetch_emails(last_timestamp)
+    if new_emails:
+        save_to_json(new_emails, OUTPUT_FILE)
+        save_timestamp(new_timestamp, LAST_TIMESTAMP_FILE)
+        print(f"Processed {len(new_emails)} new emails.")
+    else:
+        print("No new emails since the last run.")
